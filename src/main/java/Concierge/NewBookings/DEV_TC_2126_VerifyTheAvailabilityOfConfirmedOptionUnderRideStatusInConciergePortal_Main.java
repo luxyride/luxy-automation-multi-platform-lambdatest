5ac9d41dffc905f044e9aee833af2764413643af
nshyamyadav <116665603+nshyamyadav@users.noreply.github.com>
2024-12-12 15:25:00 +0530

    new testcases updated

---
+++ src/main/java/Concierge/NewBookings/DEV_TC_2126_VerifyTheAvailabilityOfConfirmedOptionUnderRideStatusInConciergePortal_Main.java
@@ -0,0 +1,222 @@
+package Concierge.NewBookings;
+
+import java.util.Date;
+import java.util.List;
+import common.TestBase;
+import java.util.Locale;
+import java.time.Duration;
+import java.util.Calendar;
+import java.time.LocalDate;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import common.GetCurrentDateTime;
+import java.text.SimpleDateFormat;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import common.UpdateExtentReportResults;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.interactions.Actions;
+import com.relevantcodes.extentreports.LogStatus;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class DEV_TC_2126_VerifyTheAvailabilityOfConfirmedOptionUnderRideStatusInConciergePortal_Main extends TestBase {
+
+	WebDriver driver;
+	Actions action;
+	String expected;
+	String testStatus;
+	String scenario;
+	String testStep;
+	Boolean visibilityStatus;
+	String screenshotPath;
+	String exception;
+	int indexCount = 1;
+
+	@FindBy(xpath = "//input[@type='text']")
+	WebElement eMailInput;
+
+	@FindBy(xpath = "//input[@type='password']")
+	WebElement passwordInput;
+
+	@FindBy(xpath = "//i[@class='fa fa-eye']")
+	WebElement eyeIcon;
+
+	@FindBy(xpath = "//button[normalize-space()='Sign In']")
+	WebElement signInBtn_Login;
+
+	@FindBy(xpath = "(//button[@type='button'])[1]")
+	WebElement signInBtnDropdown;
+
+	@FindBy(xpath = "(//span[text() = 'New Booking'])[1]")
+	WebElement newBookingTab;
+
+	@FindBy(xpath = "(//span[text() = 'Rides'])[1]")
+	WebElement ridesSection;
+
+	@FindBy(xpath = "(//span[text() = 'All'])[1]")
+	WebElement ridesTabAll;
+	
+	@FindBy(xpath = "//div[@class='css-151xaom-placeholder']")
+	WebElement dropdown;
+	
+	@FindBy(xpath = "//div[@class='css-dvua67-singleValue']")
+	WebElement confirmed;
+
+	LocalDate today;
+	TestBase objTestBase;
+	JavascriptExecutor js;
+	GetCurrentDateTime getDate;
+	UpdateExtentReportResults objupdateResults;
+	Date date;
+	SimpleDateFormat formatDate;
+	String currDate;
+
+	public DEV_TC_2126_VerifyTheAvailabilityOfConfirmedOptionUnderRideStatusInConciergePortal_Main(WebDriver driver) {
+		try {
+			this.driver = driver;
+			PageFactory.initElements(driver, this);
+			objTestBase = new TestBase();
+			getDate = new GetCurrentDateTime();
+			objupdateResults = new UpdateExtentReportResults(driver);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void eMailInput() {
+		try {
+			action = new Actions(driver);
+			eMailInput.sendKeys(prop.getProperty("conciergeadminMail"));
+			action.sendKeys(Keys.TAB).build().perform();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void passwordInput() {
+		try {
+			action = new Actions(driver);
+			passwordInput.sendKeys(prop.getProperty("conciergePwd"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void eyeIconClick() {
+		try {
+			action = new Actions(driver);
+			action.moveToElement(eyeIcon).click().build().perform();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfSigninButton(Boolean visibilityStatus) {
+		try {
+			if (signInBtn_Login.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public void clickSigninButton() {
+		try {
+			action = new Actions(driver);
+			action.moveToElement(signInBtn_Login).click().build().perform();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfLoggedinUser(Boolean visibilityStatus) {
+		try {
+			objTestBase.defaultWaitTime(2000);
+			if (signInBtnDropdown.isDisplayed()) {
+				expected = signInBtnDropdown.getText();
+				if (expected.toLowerCase().contains("welcome")) {
+					visibilityStatus = true;
+				} else {
+					visibilityStatus = false;
+				}
+			} else {
+				visibilityStatus = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public Boolean visibilitOfNewBookingTab(Boolean visibilityStatus) {
+		try {
+			if (newBookingTab.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public void clickNewBooking() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(newBookingTab).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void clickOnRidesSection() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(ridesSection).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(ridesTabAll).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfDropDown(Boolean visibilityStatus) {
+		try {
+			if (confirmed.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public Boolean visibilityOfDropDownConfirmedValue(Boolean visibilityStatus) {
+		try {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollIntoView();", confirmed);
+			if (confirmed.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+
+}
---
+++ src/test/java/Concierge/NewBookings/DEV_TC_2129_VerifyTheFucntionalityOfConfirmedOptionUnderRideStatusInConciergePortal_Test.java
@@ -0,0 +1,231 @@
+package Concierge.NewBookings;
+
+import java.util.Map;
+import common.TestBase;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import common.UpdateExtentReportResults;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class DEV_TC_2129_VerifyTheFucntionalityOfConfirmedOptionUnderRideStatusInConciergePortal_Test extends TestBase {
+
+	String currURL;
+	String screenshotPath;
+	String expected;
+	String userName;
+	String testStep;
+	String testStatus;
+	WebElement element;
+	Actions action;
+	Boolean visibilityStatus;
+	String exception = "";
+	int vechilesCount = 0;
+	String scenario;
+	Boolean bookingStatus;
+	JavascriptExecutor js;
+	String tripID;
+
+	DEV_TC_2129_VerifyTheFucntionalityOfConfirmedOptionUnderRideStatusInConciergePortal_Main objVerifyNewBookingConciergeMain;
+	DEV_TC_2129_VerifyTheFucntionalityOfConfirmedOptionUnderRideStatusInConciergePortal_Test objVerifyNewBookingConciergeTest;
+	UpdateExtentReportResults objupdateResults;
+	TestBase objTestBase;
+
+	@BeforeClass(groups = "Concierge.NewBookings")
+	@Parameters({ "browser" })
+	public void beforeClass(String browser) {
+		try {
+			objTestBase = new TestBase();
+			driver = objTestBase.launchBrowser(browser, driver, getClass().getSimpleName());
+
+			// Clear the TripIDs which were booked from DEV_TC_1126:
+			rideBookingIdsConcierge.clear();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Test(groups = "Concierge.NewBookings")
+	public void verificationOfNewBookingFromDispatchSite() throws Exception {
+
+		try {
+			action = new Actions(driver);
+			objVerifyNewBookingConciergeMain = new DEV_TC_2129_VerifyTheFucntionalityOfConfirmedOptionUnderRideStatusInConciergePortal_Main(
+					driver);
+			objVerifyNewBookingConciergeTest = new DEV_TC_2129_VerifyTheFucntionalityOfConfirmedOptionUnderRideStatusInConciergePortal_Test();
+			objTestBase = new TestBase();
+			objupdateResults = new UpdateExtentReportResults(driver);
+
+			logger = extent.startTest(objVerifyNewBookingConciergeTest.getClass().getSimpleName());
+			utillLogger.info("## " + objVerifyNewBookingConciergeTest.getClass().getSimpleName()
+					+ " - START --------------------------------------------------------------##");
+
+			driver.get(prop.getProperty("conciergeURL"));
+			driver.manage().window().maximize();
+			objTestBase.defaultWaitTime(3000);
+
+			currURL = driver.getCurrentUrl();
+			testStep = "Verify concierge Homepage Launch";
+
+			if (currURL.toLowerCase().contains(prop.getProperty("environment"))
+					&& currURL.toLowerCase().contains("luxyride")) {
+				if (localExecutionFlag == true)
+					objupdateResults.updateResults(screenshotPath, logger, LogStatus.PASS, testStep, exception);
+				else
+					lambdaTestStatusUpdate("passed", testStep);
+				testStatus = "PASSED";
+			} else {
+				if (localExecutionFlag == true)
+					objupdateResults.updateResults(screenshotPath, logger, LogStatus.FAIL, testStep, exception);
+				else
+					lambdaTestStatusUpdate("failed", testStep);
+				testStatus = "FAILED";
+			}
+			// ----------------------------------------------------------------------
+			testStep = "Verify the entered Concierge User Logins";
+			if (testStatus == "PASSED") {
+				testStatus = " ";
+				objTestBase.defaultWaitTime(2000);
+				objVerifyNewBookingConciergeMain.eMailInput();
+				objTestBase.defaultWaitTime(2000);
+				objVerifyNewBookingConciergeMain.passwordInput();
+				objTestBase.defaultWaitTime(2000);
+				objVerifyNewBookingConciergeMain.eyeIconClick();
+				objTestBase.defaultWaitTime(1000);
+
+				visibilityStatus = objVerifyNewBookingConciergeMain.visibilityOfSigninButton(visibilityStatus);
+				if (visibilityStatus.booleanValue() == true) {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.PASS, testStep, exception);
+					else
+						lambdaTestStatusUpdate("passed", testStep);
+					objVerifyNewBookingConciergeMain.clickSigninButton();
+					objTestBase.defaultWaitTime(3000);
+					testStatus = "PASSED";
+				} else {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.FAIL, testStep, exception);
+					else
+						lambdaTestStatusUpdate("failed", testStep);
+					testStatus = "FAILED";
+				}
+
+				visibilityStatus = objVerifyNewBookingConciergeMain.visibilityOfLoggedinUser(visibilityStatus);
+				testStep = "Verification of Dispatch Login";
+				if (visibilityStatus.booleanValue() == true) {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.PASS, testStep, exception);
+					else
+						lambdaTestStatusUpdate("passed", testStep);
+					testStatus = "PASSED";
+				} else {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.FAIL, testStep, exception);
+					else
+						lambdaTestStatusUpdate("failed", testStep);
+					testStatus = "FAILED";
+				}
+				
+				objTestBase.defaultWaitTime(2000);
+				objVerifyNewBookingConciergeMain.clickNewBooking();
+				objTestBase.defaultWaitTime(3000);
+				objVerifyNewBookingConciergeMain.clickOnRidesSection();
+				objTestBase.defaultWaitTime(3000);
+
+				visibilityStatus = objVerifyNewBookingConciergeMain.visibilityOfDropDown(visibilityStatus);
+				testStep = "Verification of Dropdown values";
+				if (visibilityStatus.booleanValue() == true) {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.PASS, testStep, exception);
+					else
+						lambdaTestStatusUpdate("passed", testStep);
+					testStatus = "PASSED";
+				} else {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.FAIL, testStep, exception);
+					else
+						lambdaTestStatusUpdate("failed", testStep);
+					testStatus = "FAILED";
+				}
+				
+				visibilityStatus = objVerifyNewBookingConciergeMain.visibilityOfDropDownConfirmedValue(visibilityStatus);
+				testStep = "Verification of dropdown confirmed value";
+				if (visibilityStatus.booleanValue() == true) {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.PASS, testStep, exception);
+					else
+						lambdaTestStatusUpdate("passed", testStep);
+					testStatus = "PASSED";
+				} else {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.FAIL, testStep, exception);
+					else
+						lambdaTestStatusUpdate("failed", testStep);
+					testStatus = "FAILED";
+				}
+				
+				objTestBase.defaultWaitTime(3000);
+				objVerifyNewBookingConciergeMain.clickOnconfirmedValue();
+				objTestBase.defaultWaitTime(3000);
+				
+				visibilityStatus = objVerifyNewBookingConciergeMain.visibilityOfConfirmedValueRides(visibilityStatus);
+				testStep = "Verification of dropdown confirmed value Rides page";
+				if (visibilityStatus.booleanValue() == true) {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.PASS, testStep, exception);
+					else
+						lambdaTestStatusUpdate("passed", testStep);
+					testStatus = "PASSED";
+				} else {
+					if (localExecutionFlag == true)
+						objupdateResults.updateResults(screenshotPath, logger, LogStatus.FAIL, testStep, exception);
+					else
+						lambdaTestStatusUpdate("failed", testStep);
+					testStatus = "FAILED";
+				}
+			}
+			
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@AfterClass(groups = "Concierge.NewBookings")
+	public void afterClass() {
+		driver.quit();
+
+		// Removes the Duplicate Trip IDs from rideBookingIdsConcierge HashMap
+		final Iterator<Entry<String, String>> iter = rideBookingIdsConcierge.entrySet().iterator();
+		final HashSet<String> valueSet = new HashSet<String>();
+		while (iter.hasNext()) {
+			final Entry<String, String> next = iter.next();
+			if (!valueSet.add(next.getValue())) {
+				iter.remove();
+			}
+		}
+
+		// Print the Captured the Ride Booking ids - captured from
+		// DEV_TC_1248_VerifyNewRideBookingFunctionalityFromConciergePortal_Test:
+		utillLogger.info("//--------------------------------------------------------------------------------------//");
+		for (Map.Entry<String, String> entry : rideBookingIdsConcierge.entrySet()) {
+			System.out
+					.println("Booking IDs from TC: DEV_TC_1248_VerifyNewRideBookingFunctionalityFromConciergePortal = "
+							+ "Total TripIDs = " + rideBookingIdsConcierge.size() + " - " + entry.getKey() + " - "
+							+ entry.getValue());
+			utillLogger.info("Booking IDs from TC: DEV_TC_1248_VerifyNewRideBookingFunctionalityFromConciergePortal = "
+					+ "Total TripIDs = " + rideBookingIdsConcierge.size() + " - " + entry.getKey() + " - "
+					+ entry.getValue());
+		}
+		utillLogger.info("//--------------------------------------------------------------------------------------//");
+		utillLogger.info("## " + objVerifyNewBookingConciergeTest.getClass().getSimpleName()
+				+ " - END --------------------------------------------------------------##");
+	}
+}
