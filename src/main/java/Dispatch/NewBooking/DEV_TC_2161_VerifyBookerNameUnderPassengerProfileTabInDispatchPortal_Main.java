f7bc2c5bfbb20415e6801f09410e53e546319e3e
nshyamyadav <116665603+nshyamyadav@users.noreply.github.com>
2025-03-12 11:23:40 +0530

    updated new testcases

---
+++ src/main/java/Dispatch/NewBooking/DEV_TC_2161_VerifyBookerNameUnderPassengerProfileTabInDispatchPortal_Main.java
@@ -0,0 +1,216 @@
+package Dispatch.NewBooking;
+
+import common.TestBase;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.interactions.Actions;
+
+public class DEV_TC_2161_VerifyBookerNameUnderPassengerProfileTabInDispatchPortal_Main extends TestBase {
+
+	WebDriver driver;
+	Actions action;
+	String expected;
+
+	@FindBy(xpath = "//input[@type='text']")
+	WebElement eMailInput;
+
+	@FindBy(xpath = "//input[@type='password']")
+	WebElement passwordInput;
+
+	@FindBy(xpath = "//i[@class='fa fa-eye']")
+	WebElement eyeIcon;
+
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement signInBtn;
+
+	@FindBy(xpath = "//button[contains(text(),'Welcome')]")
+	WebElement signinUser;
+
+	@FindBy(xpath = "(//span[normalize-space()='New Booking'])[1]")
+	WebElement newbooking;
+
+	@FindBy(xpath = "(//span[normalize-space()='Passengers'])[1]")
+	WebElement passengerTab;
+
+	@FindBy(xpath = "//h5[@class='PageHeading-driver-profile profileName']")
+	WebElement profilePage;
+
+	@FindBy(xpath = "//th[@title='Booker Name']")
+	WebElement bookername;
+	
+	@FindBy(xpath = "//input[@placeholder='Search']")
+	WebElement searchInput;
+	
+	@FindBy(xpath = "(//i[@title='Passenger Profile'])[1]")
+	WebElement profileIcon;
+	
+	
+
+	TestBase objTestBase;
+
+	public DEV_TC_2161_VerifyBookerNameUnderPassengerProfileTabInDispatchPortal_Main(WebDriver driver) {
+		try {
+			this.driver = driver;
+			PageFactory.initElements(driver, this);
+			objTestBase = new TestBase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void eMailInput() {
+		try {
+			action = new Actions(driver);
+			eMailInput.sendKeys(prop.getProperty("dispatchadminMail"));
+			action.sendKeys(Keys.TAB).build().perform();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public void passwordInput() {
+		try {
+			action = new Actions(driver);
+			passwordInput.sendKeys(prop.getProperty("dispatchadminPwd"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public void eyeIconClick() {
+		try {
+			action = new Actions(driver);
+			action.moveToElement(eyeIcon).click().build().perform();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public Boolean visibilityOfSigninButton(Boolean visibilityStatus) {
+		try {
+			if (signInBtn.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return visibilityStatus;
+	}
+
+	public void clickSigninButton() {
+		try {
+			signInBtn.click();
+			// Validate Dispatch 2fa Authentication:
+			Boolean visibilityStatus = false;
+			visibilityStatus = validateDispatchBypassCode(visibilityStatus);
+			utillLogger.info(
+					"2FA Validation Status for Script - " + getClass().getSimpleName() + " = " + visibilityStatus);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void clickNewBooking() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(newbooking).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfLoggedinUser(Boolean visibilityStatus) {
+		try {
+			waitTimeForElement(signinUser);
+			if (signinUser.isDisplayed()) {
+				objTestBase.defaultWaitTime(1000);
+				if (signinUser.getText().toLowerCase().contains("welcome")) {
+					visibilityStatus = true;
+				} else {
+					visibilityStatus = false;
+				}
+			} else {
+				visibilityStatus = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public void clickPassengerTab() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(passengerTab).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfPassangerProfilePage(Boolean visibilityStatus) {
+		try {
+			waitTimeForElement(profilePage);
+			if (profilePage.isDisplayed()) {
+				objTestBase.defaultWaitTime(1000);
+				if (profilePage.getText().toLowerCase().contains("profile")) {
+					visibilityStatus = true;
+				} else {
+					visibilityStatus = false;
+				}
+			} else {
+				visibilityStatus = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public Boolean visibilityOfBookerName(Boolean visibilityStatus) {
+		try {
+			if (bookername.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return visibilityStatus;
+	}
+
+	public void enterSearchInput() {
+		try {
+			action = new Actions(driver);
+			searchInput.sendKeys(prop.getProperty("spEmail"));
+			action.sendKeys(Keys.TAB).build().perform();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public void clickOnProfileIcon() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(profileIcon).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
