16f4c2dd98aed9531f1a31617b653068132bbe62
suryateja26 <83082152+suryateja26@users.noreply.github.com>
2025-04-16 22:35:13 +0530

    updated

---
+++ src/main/java/Dispatch/NewBooking/DEV_TC_2236_VerifyTheAvailabilityOfIataFieldUnderTaProfileInDispatchPortal_Main.java
@@ -0,0 +1,194 @@
+package Dispatch.NewBooking;
+
+import common.TestBase;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.interactions.Actions;
+
+public class DEV_TC_2236_VerifyTheAvailabilityOfIataFieldUnderTaProfileInDispatchPortal_Main extends TestBase {
+
+	WebDriver driver;
+	Actions action;
+	String expected;
+
+	@FindBy(xpath = "//input[@type='text']")
+	WebElement eMailInput;
+
+	@FindBy(xpath = "//input[@type='password']")
+	WebElement passwordInput;
+
+	@FindBy(xpath = "//i[@class='fa fa-eye']")
+	WebElement eyeIcon;
+
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement signInBtn;
+
+	@FindBy(xpath = "//button[contains(text(),'Welcome')]")
+	WebElement signinUser;
+
+	@FindBy(xpath = "(//span[normalize-space()='New Booking'])[1]")
+	WebElement newbooking;
+	
+	@FindBy(xpath = "(//span[normalize-space()='Travel Arranger'])[1]")
+	WebElement travelArranger;
+	
+	@FindBy(xpath = "(//span[normalize-space()='Travel Arranger'])[2]")
+	WebElement travelArranger2;
+	
+	@FindBy(xpath = "(//a[normalize-space()='Add Travel Arranger'])[1]")
+	WebElement addTaBtn;
+	
+	@FindBy(xpath = "(//input[@type='text'])[6]")
+	WebElement iataField;
+	
+	
+
+	TestBase objTestBase;
+
+	public DEV_TC_2236_VerifyTheAvailabilityOfIataFieldUnderTaProfileInDispatchPortal_Main(WebDriver driver) {
+		try {
+			this.driver = driver;
+			PageFactory.initElements(driver, this);
+			objTestBase = new TestBase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void eMailInput() {
+		try {
+			action = new Actions(driver);
+			eMailInput.sendKeys(prop.getProperty("dispatchadminMail"));
+			action.sendKeys(Keys.TAB).build().perform();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public void passwordInput() {
+		try {
+			action = new Actions(driver);
+			passwordInput.sendKeys(prop.getProperty("dispatchadminPwd"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public void eyeIconClick() {
+		try {
+			action = new Actions(driver);
+			action.moveToElement(eyeIcon).click().build().perform();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	public Boolean visibilityOfSigninButton(Boolean visibilityStatus) {
+		try {
+			if (signInBtn.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return visibilityStatus;
+	}
+
+	public void clickSigninButton() {
+		try {
+			signInBtn.click();
+			// Validate Dispatch 2fa Authentication:
+			Boolean visibilityStatus = false;
+			visibilityStatus = validateDispatchBypassCode(visibilityStatus);
+			utillLogger.info(
+					"2FA Validation Status for Script - " + getClass().getSimpleName() + " = " + visibilityStatus);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void clickNewBooking() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(newbooking).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfLoggedinUser(Boolean visibilityStatus) {
+		try {
+			waitTimeForElement(signinUser);
+			if (signinUser.isDisplayed()) {
+				objTestBase.defaultWaitTime(1000);
+				if (signinUser.getText().toLowerCase().contains("welcome")) {
+					visibilityStatus = true;
+				} else {
+					visibilityStatus = false;
+				}
+			} else {
+				visibilityStatus = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return visibilityStatus;
+	}
+
+	public void clickTravelArranger() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(travelArranger).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void clickTravelArranger2() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(travelArranger2).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void clickAddTravelArrangerBtn() {
+		try {
+			action = new Actions(driver);
+			objTestBase.defaultWaitTime(1000);
+			action.moveToElement(addTaBtn).click().build().perform();
+			objTestBase.defaultWaitTime(1000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public Boolean visibilityOfIATAField(Boolean visibilityStatus) {
+		try {
+			if (iataField.isDisplayed())
+				visibilityStatus = true;
+			else
+				visibilityStatus = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return visibilityStatus;
+	}
+}
